25b893706bfae52c53c5a4ae34fe2790448ddebe
cpyoufy <cpyoufy@gmail.com>
2021-06-23 09:50:59 +0800

    Initial Commit

---
+++ Introduction/IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionApp.swift
+//  Introduction
+//
+//  Created by rgs on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
